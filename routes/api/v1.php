55fc2e3a0697e592845acc009e5be7db95c28b8f
Walid Said <wsaid@github.com>
2022-04-08 21:05:31 -0400

    Add api version

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1ProductsIndexController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('products')->as('products:')->group(function() {
+
+    Route::get('/', V1ProductsIndexController::class)->name('show');
 });
