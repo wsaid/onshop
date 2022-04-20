71b3f149b26e2f18623ff539fad4f3a0282b5f2e
Walid Said <wsaid@github.com>
2022-04-20 19:08:21 -0400

    Enforce morph map

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Domains\Catalog\Models\Variant;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Relation::enforceMorphMap([
+            'variant' => Variant::class
+        ]);
     }
 }
