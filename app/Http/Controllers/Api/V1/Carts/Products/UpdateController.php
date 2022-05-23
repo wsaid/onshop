b6f17a0865430a5224a52323b35021e9511a5b41
Walid Said <wsaid@github.com>
2022-05-23 19:51:19 -0400

    Remove unused imports

--- app/Http/Controllers/Api/V1/Carts/Products/UpdateController.php
+++ app/Http/Controllers/Api/V1/Carts/Products/UpdateController.php
@@ -5,10 +5,8 @@ namespace App\Http\Controllers\Api\V1\Carts\Products;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\Carts\Products\UpdateRequest;
 use Domains\Customer\Actions\ChangeCartItemQuantity;
-use Domains\Customer\Aggregates\CartAggregate;
 use Domains\Customer\Models\Cart;
 use Domains\Customer\Models\CartItem;
-use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
